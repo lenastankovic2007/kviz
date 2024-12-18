78f8272e515c70d96bc827aa12616f6d5bc80a4e
TMarinkovic <tamara.m.cica@gmail.com>
2024-12-19 00:20:07 +0100

    klasa Question

--- kviz/kviz/Question.cs
+++ kviz/kviz/Question.cs
@@ -11,6 +11,9 @@ namespace kviz
         public List<string> Answers { get; private set; }
         public int CorrectIndex { get; private set; }
         public int PointValue { get; private set; }
+        public int ID {get; private set;}
+
+        private static QuestionBank _questionBank;
 
         public string Display(int n)
         {
@@ -25,7 +28,39 @@ namespace kviz
             }
             return WholeText;
         
+         public override string ToString()
+    {
+        string answersString = string.Join(",", Answers);
+        return $"{Text}|{answersString}|{CorrectIndex}|{PointValue}";
+
+        public Question {string Text<string> Answers, int CorrectIndex, int PointValue, int ID}
+    }
+        var random = new Random();
+        int newId;
+      while (questionBank.QuestionExists(newId));  
+        {
+            newId = random.Next(1, int.MaxValue);
+        }
+        ID = newId;
+        Text = text;
+        Answers = answers;
+        CorrectAnswer = correctAnswer;
+         PointValue=pointvalue;
+    }
+
+    public Question(string questionString)
+    {
+        var parts = questionString.Split(',');
+        if (parts.Length > 1)
+        {
+            ID = int.Parse(parts[0]);
+            Text = parts[1];
+            Answers = new List<string>(); 
+            CorrectAnswer = "";
+        }
+    }
         
+
         }
 
 
@@ -33,4 +68,3 @@ namespace kviz
 
 
     }
-}
